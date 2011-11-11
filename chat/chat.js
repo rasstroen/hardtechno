e4c78cc570d7fbe29c983d6da73027d9d1d84b4f
Mihail Chubar <amuhc@ya.ru>
2011-11-11 11:33:02 +0400

    fix: chat

--- chat/chat.js
+++ chat/chat.js
@@ -1,200 +1,230 @@
 var chat = {
-        //
-        // settings
-        //
-        // where is server
-        server_url:'/',
-        // cookie name with auth hash
-        authCookieName:'thardhash_',
-        // cookie name with user id
-        authCookieIdName:'thardid_',
-        // chat refresh interval in seconds
-        refreshSpeed: 1.8,
-        // translates
-        translate_say : 'написать',
-        // variables
-        //
-        // can i write?
-        can_write : false,
-        // chat status
-        status : 1,
-        // statuses
-        status_wait : 1, // do nothing
-        status_request_sended_fetch : 2, // sent request fo fetch new data
-        status_request_sended_message : 3, // sent message
-        // last message user see now
-        last_message_received_id : 0,
-        // last message timestamp
-        last_message_received_time : 0,
-        // messages
-        messages : {},
-        // element on page for draw chat
-        divElement:false,
-        // element on page for draw message before it
-        last_message_div:false,
-        // element fo logging
-        logElement:false,
-        // secret for authorization
-        authCookie:'',
-        // user's id
-        authId:'',
-        // user's profile
-        profile:{},
-        // initializing
-        init : function(id){
-                chat.divElement = document.getElementById(id);
-                chat.last_message_div = document.createElement('div');
-                chat.divElement.appendChild(chat.last_message_div);
-                chat.authCookie = chat.getCookie(chat.authCookieName);
-                chat.authId = chat.getCookie(chat.authCookieIdName);
-                chat.authorize();
-        },
-        log: function(s){
-                if(!chat.logDiv)	{
-                        chat.logDiv = document.createElement('DIV');
-                        chat.logDiv.id = 'chat_log';
-                        chat.divElement.appendChild(chat.logDiv);
-                }
-                chat.logDiv.innerHTML += "\n<br>"+s;
-        },
-        // authorize user via server
-        authorize : function(){
-                data = {};
-                data.action = 'authorize';
-                data.id = chat.authId;
-                data.secret = chat.authCookie;
-                chat.request(data, chat.on_authorize);
-        },
-        drawInput : function(){
-                var _input = document.createElement('input');
-                _input.type = 'text';
-                _input.id = 'chat_input';
-                chat.divElement.appendChild(_input);
+	//
+	// settings
+	//
+	// where is server
+	server_url:'/',
+	// cookie name with auth hash
+	authCookieName:'thardhash_',
+	// cookie name with user id
+	authCookieIdName:'thardid_',
+	// chat refresh interval in seconds
+	refreshSpeed: 3.8,
+	// translates
+	translate_say : 'написать',
+	// variables
+	//
+	// can i write?
+	can_write : false,
+	// chat status
+	status : 1,
+	// statuses
+	status_wait : 1, // do nothing
+	status_request_sended_fetch : 2, // sent request fo fetch new data
+	status_request_sended_message : 3, // sent message
+	// last message user see now
+	last_message_received_id : 0,
+	// last message timestamp
+	last_message_received_time : 0,
+	// messages
+	messages : {},
+	// element on page for draw chat
+	divElement:false,
+	// container for message input & button
+	send_plank_div:false,
+	// input area
+	chat_input:false,
+	// send button
+	chat_submit:false,
+	// secret for authorization
+	authCookie:'',
+	// user's id
+	authId:'',
+	// user's profile
+	profile:{},
+	// initializing
+	init : function(id){
+		chat.divElement = document.getElementById(id);
+		chat.divElement.innerHTML = '';
+		chat.messages = {};
+		chat.profile = {};
+		chat.status = 1;
+		chat.last_message_received_id = 0;
+		chat.last_message_received_time = 0;
+		chat.authCookie = chat.getCookie(chat.authCookieName);
+		chat.authId = chat.getCookie(chat.authCookieIdName);
+		chat.authorize();
+	},
+	// authorize user via server
+	authorize : function(){
+		data = {};
+		data.action = 'authorize';
+		data.id = chat.authId;
+		data.secret = chat.authCookie;
+		chat.request(data, chat.on_authorize);
+	},
+	clickButton : function(e) {
+		var keynum;
+		var keychar;
+		var numcheck;
+		if(window.event){
+			keynum = e.keyCode
+		}
+		else if(e.which){
+			keynum = e.which
+		}
+		if(keynum == 13){
+			chat.send();
+		}
+	},
+	drawInput : function(){
+		chat.send_plank_div = document.createElement('DIV');
+		chat.send_plank_div.id = 'chat_send_plank_div';
+		chat.divElement.appendChild(chat.send_plank_div);
+		
+		var _input = document.createElement('input');
+		_input.type = 'text';
+		_input.id = 'chat_input';
+		chat.send_plank_div.appendChild(_input);
+		chat.chat_input = _input;
+		chat.chat_input.onkeypress = function(event){
+			chat.clickButton(event)
+		};
+		
+		var _button = document.createElement('input');
+		_button.type = 'button';
+		_button.id = 'chat_submit';
+		_button.value = chat.translate_say;
+		_button.onclick = function(){
+			chat.send();
+		}
+		chat.chat_submit = _button;
+		chat.send_plank_div.appendChild(_button);
+		
+		chat.chat_input.focus();
+	},
+	set_authorized : function (is_authorized){
+		if(is_authorized)	{
+			chat.can_write = 1;
+			chat.drawInput();
+		}else{
+                        
+	}
+	},
+	on_authorize : function(data){
+		if(data && data.success){
+			// authorized, so we can wrote
+			chat.profile = data.profile;
+			chat.set_authorized(true);
+		}else
+			chat.set_authorized(false);
+		chat.start_timer();
+	},
+	start_timer : function(){
+		chat.on_timer();
+		setInterval('chat.on_timer()',chat.refreshSpeed*1000);
+	},
+	on_timer: function(){
+		// if it's no pending get requests
+		// put new request
+		if(chat.status == chat.status_wait){
+			// we can sent request to fetch data
+			chat.get();
+		}
+	},
+	send : function(){
+		chat.status = chat.status_request_sended_message;
+		var data = {};
+		data.action = 'say';
+		data.message = document.getElementById('chat_input').value;
+		// message not empty & we are authorized to write
+		if(data.message && chat.can_write){
+			chat.chat_input.disabled = 'disabled';
+			chat.request(data, chat.on_after_send);
+		}
+		chat.chat_input.value = '';
+	},
+	// request for get messages
+	get : function(){
+		var data = {};
+		data.action = 'fetch';
+		data.last_message_received_id = chat.last_message_received_id;
+		data.last_message_received_time = chat.last_message_received_time;
+		chat.status = chat.status_request_sended_fetch;
+		chat.request(data, chat.on_after_get);
+	},
+	on_after_get : function(data){
+		if(data && data['success']){
+			if(data.messages){
+				for(var i in data.messages){
+					if(!chat.messages[data.messages[i].id]){
+						// new message
+						chat.messages[data.messages[i].id] = data.messages[i];
+						chat.draw_message(data.messages[i]);
+						chat.last_message_received_id = Math.max(chat.last_message_received_id, data.messages[i].id)
+						chat.last_message_received_time = Math.max(chat.last_message_received_time, data.messages[i].time)
+					}
+				}
+			}
+			if(data.refresh){
+				chat.init(chat.divElement.id)
+			}
+			if(data.last_message_id>-1){
+				chat.last_message_received_id = data.last_message_id;
+			}
+		}
+		chat.status = chat.status_wait;
+	},
+	// after send request to server
+	on_after_send : function(data){
+		chat.status = chat.status_wait;
+		chat.last_message_received_id = data.last_message_id;
+		chat.on_after_get(data);
+		chat.chat_input.disabled = '';
+	},
+	// inserting message div by message object into chat window
+	draw_message : function(message){
+		var message_plank = document.createElement('div');
+		message_plank.id = 'message_'+message.id;
+		message_plank.name = 'chat_message';
+		message_plank.innerHTML = message.message;
+		// find place
+		chat.divElement.appendChild(message_plank);
+	},
+	// getting browser's Cookie
+	getCookie : function (c_name){
+		var i,x,y,ARRcookies=document.cookie.split(";");
+		for (i=0 ; i<ARRcookies.length; i++){
+			x = ARRcookies[i].substr(0 , ARRcookies[i].indexOf("="));
+			y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+			x = x.replace(/^\s+|\s+$/g , "");
+			if (x == c_name)
+				return unescape(y);
+		}
+		return false;
+	},
+	// making request
+	request : function(data , callback){
+		callback = callback ? callback : function(data){};
+		data.jquery = 'chat_module';
 
-                var _button = document.createElement('input');
-                _button.type = 'button';
-                _button.value = chat.translate_say;
-                _button.onclick = function(){
-                        chat.send();
-                }
-                chat.divElement.appendChild(_button);
-        },
-        set_authorized : function (is_authorized){
-                if(is_authorized)	{
-                        chat.log('authorized as '+chat.profile.nick);
-                        chat.can_write = 1;
-                        chat.drawInput();
-                }else{
-                        chat.log('unauthorized');
-                }
-        },
-        on_authorize : function(data){
-                if(data && data.success){
-                        // authorized, so we can wrote
-                        chat.profile = data.profile;
-                        chat.set_authorized(true);
-                }else
-                        chat.set_authorized(false);
-                chat.start_timer();
-        },
-        start_timer : function(){
-                chat.on_timer();
-                setInterval('chat.on_timer()',chat.refreshSpeed*1000);
-        },
-        on_timer: function(){
-                // if it's no pending get requests
-                // put new request
-                if(chat.status == chat.status_wait){
-                        // we can sent request to fetch data
-                        chat.get();
-                }
-        },
-        send : function(){
-                chat.status = chat.status_request_sended_message;
-                var data = {};
-                data.action = 'say';
-                data.message = document.getElementById('chat_input').value;
-                // message not empty & we are authorized to write
-                if(data.message && chat.can_write){
-                        chat.request(data, chat.on_after_send);
-                }
-        },
-        // request for get messages
-        get : function(){
-                var data = {};
-                data.action = 'fetch';
-                data.last_message_received_id = chat.last_message_received_id;
-                data.last_message_received_time = chat.last_message_received_time;
-                chat.status = chat.status_request_sended_fetch;
-                chat.request(data, chat.on_after_get);
-        },
-        on_after_get : function(data){
-                if(data && data['success']){
-                        if(data.messages){
-                                for(var i in data.messages){
-                                        if(!chat.messages[data.messages[i].id]){
-                                                // new message
-                                                chat.messages[data.messages[i].id] = data.messages[i];
-                                                chat.draw_message(data.messages[i]);
-                                                chat.last_message_received_id = Math.max(chat.last_message_received_id, data.messages[i].id)
-                                                chat.last_message_received_time = Math.max(chat.last_message_received_time, data.messages[i].time)
-                                        }
-                                }
-                        }
-                }
-                chat.status = chat.status_wait;
-        },
-        // after send request to server
-        on_after_send : function(data){
-                chat.status = chat.status_wait;
-                chat.last_message_received_id = data.last_message_id;
-                chat.on_after_get(data);
-        },
-        // inserting message div by message object into chat window
-        draw_message : function(message){
-                var message_plank = document.createElement('div');
-                message_plank.id = 'message_'+message.id;
-                message_plank.name = 'chat_message';
-                message_plank.innerHTML = message.message;
-                // find place
-                chat.divElement.appendChild(message_plank);
-        },
-        // getting browser's Cookie
-        getCookie : function (c_name){
-                var i,x,y,ARRcookies=document.cookie.split(";");
-                for (i=0 ; i<ARRcookies.length; i++){
-                        x = ARRcookies[i].substr(0 , ARRcookies[i].indexOf("="));
-                        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-                        x = x.replace(/^\s+|\s+$/g , "");
-                        if (x == c_name)
-                                return unescape(y);
-                }
-                return false;
-        },
-        // making request
-        request : function(data , callback){
-                callback = callback ? callback : function(data){};
-                data.jquery = 'chat_module';
+		var request = $.ajax({
+			cache: false,
+			url: chat.server_url,
+			type: "POST",
+			data: data,
+			timeout: 10000,
+			dataType: "json",
+			global: false
+		});
 
-                var request = $.ajax({
-                        cache: false,
-                        url: chat.server_url,
-                        type: "POST",
-                        data: data,
-                        timeout: 10000,
-                        dataType: "json",
-                        global: false
-                });
-
-                request.done(function(data){
-                        // request done
-                        callback(data);
-                });
-                request.fail(function(o , status){
-                        // something went wrong
-                        chat.log(status);
-                        // we will try another time
-                        chat.status = chat.status_wait;
-                });
-        }
+		request.done(function(data){
+			// request done
+			callback(data);
+		});
+		request.fail(function(o , status){
+			// we will try another time
+			chat.status = chat.status_wait;
+		});
+	}
 }
--- jmodules/Jchat_module.php
+++ jmodules/Jchat_module.php
@@ -2,96 +2,115 @@
 
 class Jchat_module extends JBaseModule {
 
-        public $max_messages = 10;
+	public $max_messages = 10;
 
-        function process() {
-                global $current_user;
-                $current_user = new CurrentUser();
-                $this->data['success'] = 1;
-                switch ($_POST['action']) {
-                        case 'authorize':
-                                $this->auth();
-                                break;
-                        case 'fetch':
-                                $this->fetch();
-                                break;
-                        case 'say':
-                                $this->say();
-                                break;
-                        default:
-                                $this->error('no action #' . $_POST['action']);
-                                break;
-                }
-        }
+	function process() {
+		global $current_user;
+		$current_user = new CurrentUser();
+		$this->data['success'] = 1;
+		switch ($_POST['action']) {
+			case 'authorize':
+				$this->auth();
+				break;
+			case 'fetch':
+				$this->fetch();
+				break;
+			case 'say':
+				$this->say();
+				break;
+			default:
+				$this->error('no action #' . $_POST['action']);
+				break;
+		}
+	}
 
-        function error($s) {
-                $this->data['error'] = $s;
-                $this->data['success'] = 0;
-        }
+	function doAdminFunction($message) {
+		$message = explode(' ', $message);
+		switch ($message[0]) {
+			case 'clear':
+				Database::query('TRUNCATE `hard_chat`');
+				// clearing chat
+				$this->data['refresh'] = true;
+				return true;
+				break;
+		}
+	}
 
-        function say() {
-                global $current_user;
-                /* @var $current_user CurrentUser */
-                if (!$current_user->authorized) {
-                        $this->error('authorization required');
-                }
-                $message = trim(strip_tags($_POST['message']));
-                if (!$message) {
-                        $this->error('illegal message');
-                }
-                $query = 'INSERT INTO `hard_chat` SET
+	function error($s) {
+		$this->data['error'] = $s;
+		$this->data['success'] = 0;
+	}
+
+	function say() {
+		global $current_user;
+		/* @var $current_user CurrentUser */
+		if (!$current_user->authorized) {
+			$this->error('authorization required');
+		}
+
+		$message = trim(strip_tags($_POST['message']));
+		if (!$message) {
+			$this->error('illegal message');
+		}
+
+		// if admin executes a command
+		if ($current_user->getRole() >= User::ROLE_SITE_ADMIN && $r = $this->doAdminFunction($message)) {
+			$lid = 0;
+		} else {
+			$query = 'INSERT INTO `hard_chat` SET
                         `id_user`=' . $current_user->id . ',
                         `message`=' . Database::escape($message) . ',
                         `time`=' . time();
-                Database::query($query);
-                $lid = Database::lastInsertId();
-                $this->data = $this->getMessages($lid-1, $current_user->id);
-                $this->data['last_message_id'] = $lid;
-                $this->data['success'] = 1;
-                return;
-        }
+			Database::query($query);
+			$lid = Database::lastInsertId();
+			$this->data = $this->getMessages($lid - 1, $current_user->id);
+		}
+		$this->data['last_message_id'] = $lid;
+		$this->data['success'] = 1;
+		return;
+	}
 
-        function getMessages($from_id, $uid) {
-                $out = array();
-                $messages = Database::sql2array('SELECT * FROM `hard_chat` ORDER BY `id` DESC LIMIT ' . $this->max_messages, 'id');
-                $uids = array();
-                foreach ($messages as $id => $message) {
-                        // if id < requested last message
-                        if ($from_id && ($id <= $from_id))
-                                unset($messages[$id]);
-                        $uids[$message['id_user']] = $message['id_user'];
-                }
-                $out['messages'] = array_slice($messages, -$this->max_messages);
-                $out['users'] = $this->getMessagesUsers($uids);
-                return $out;
-        }
+	function getMessages($from_id, $uid) {
+		$out = array();
+		$messages = Database::sql2array('SELECT * FROM `hard_chat` ORDER BY `id` DESC LIMIT ' . $this->max_messages, 'id');
+		$uids = array();
+		foreach ($messages as $id => $message) {
+			// if id < requested last message
+			if ($from_id && ($id <= $from_id))
+				unset($messages[$id]);
+			$uids[$message['id_user']] = $message['id_user'];
+		}
+		$out['messages'] = array_slice($messages, -$this->max_messages);
+		$out['users'] = $this->getMessagesUsers($uids);
+		return $out;
+	}
 
-        function getMessagesUsers($uids) {
-                $users = Users::getByIdsLoaded($uids);
-                $out = array();
-                foreach ($users as $user) {
-                        $out[$user->id] = $user->getListData();
-                }
-                return $out;
-        }
+	function getMessagesUsers($uids) {
+		$users = Users::getByIdsLoaded($uids);
+		$out = array();
+		foreach ($users as $user) {
+			$out[$user->id] = $user->getListData();
+		}
+		return $out;
+	}
 
-        function fetch() {
-                global $current_user;
-                $last_received_id = max(0, (int) $_POST['last_message_received_id']);
-                $last_received_time = max(0, (int) $_POST['last_message_received_time']);
-                $this->data = $this->getMessages($last_received_id, $current_user->id);
-                $this->data['success'] = 1;
-        }
+	function fetch() {
+		global $current_user;
+		$last_received_id = max(0, (int) $_POST['last_message_received_id']);
+		$last_received_time = max(0, (int) $_POST['last_message_received_time']);
+		$this->data = $this->getMessages($last_received_id, $current_user->id);
+		$this->data['success'] = 1;
+	}
 
-        function auth() {
-                global $current_user;
-                /* @var $current_user CurrentUser */
-                if ($current_user->authorized) {
-                        $this->data['profile'] = $current_user->getListData();
-                } else {
-                        $this->error('user is not authorized');
-                        return;
-                }
-        }
+	function auth() {
+		global $current_user;
+		/* @var $current_user CurrentUser */
+		if ($current_user->authorized) {
+			$this->data['profile'] = $current_user->getListData();
+		} else {
+			$this->error('user is not authorized');
+			return;
+		}
+	}
 
 }
